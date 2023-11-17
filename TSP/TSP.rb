ada335635e33daa91f7a5ecb01317d32814fd398
PedroPortow <pedrolportow@gmail.com>
2023-11-17 14:26:52 -0300

    prim mst attempt

--- TSP/TSP.rb
+++ TSP/TSP.rb
@@ -5,11 +5,8 @@ class TSP
   
   def read_file(path)
     lines = File.readlines(path)
-    
-    # Inicializa a matriz com zeros
     @graph = Array.new(lines.length) { Array.new(lines.length, 0) }
     
-    # Preenche a matriz com as informações do arquivo
     lines.each_with_index do |line, i|
       values = line.split.map(&:to_i)
       values.each_with_index do |value, j|
@@ -19,13 +16,46 @@ class TSP
   end
   
   def print_graph
+    puts "------------------------"
     @graph.each do |row|
-      puts row.join(" ")
+      puts row.join(" - ")
     end
+    puts "------------------------"
   end
   
+  def aproxim_algo
+    mst = prim(0)
+    
+    puts mst
+  end
+  
+  def prim(start_node = 0)
+    mst = [] 
+    visited = Array.new(@graph.length, false) 
+    queue = []  
+    
+    visited[start_node] = true # começando no nó passado por param
+    queue << [start_node, start_node, 0] # precisa botar 2 vezes pq é uma matriz
+    
+    while !queue.empty?
+      queue.sort! { |a, b| a[2] <=> b[2] } # ordena pelo peso, 2 => index do peso da aresta na queue 
+      
+      current_path = queue.shift # pegando 1 elemento
+      previous_node, next_node, weight = current_path 
+
+      next if visited[next_node] 
+
+    end
+    
+    puts mst
+    mst
+  end
 end
 
 tsp = TSP.new
-tsp.read_file("./resources/tsp1_253.txt")
-tsp.print_graph
+tsp.read_file("./TSP/resources/tsp1_253.txt")
+# tsp.print_graph
+
+tsp.aproxim_algo
+
+
