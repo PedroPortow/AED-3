cded43104d9019a3d1f6af90287c55ce140f1216
PedroPortow <pedrolportow@gmail.com>
2023-12-02 18:04:59 -0300

    nearest_neighbor attempt

--- TSP/TSP.rb
+++ TSP/TSP.rb
@@ -46,6 +46,56 @@ class TSP
     { route: optimal_path, cost: optimal_cost }
   end
 
+
+  
+  def held_karp_tsp
+    nodes = (0...@nodes_quantity).to_a  
+    starting_node = 0 
+             
+    
+    memo = Array.new(@nodes_quantity) { Array.new(2**@nodes_quantity, nil) }
+    
+  end
+  
+  def nearest_neighbor_tsp(starting_node = 0)
+    current_node = starting_node
+    unvisited_nodes = (0...@nodes_quantity).to_a - [current_node] # excluindo o current_node que no caso vai ser o starting_node na primeira iteração
+    
+    path = [current_node] # começando do current_node // aqui vai ser o starting node
+
+    nearest_node = find_nearest_neighbor(current_node, unvisited_nodes)
+    
+    while unvisited_nodes.any?
+ 
+    end
+
+  end
+  
+  def find_nearest_neighbor(current_node, unvisited_nodes) 
+    #vai retornar o nó com menor caminho entre o current_node para o unvisited_nodes
+    min_distance = Float::INFINITY
+    nearest_node = nil
+  
+    unvisited_nodes.each do |unvisited_node|
+      distance = self.get_distance_between_two_nodes(current_node, unvisited_node)
+      if distance < min_distance
+        min_distance = distance
+        nearest_node = unvisited_node
+      end
+    end
+    
+    puts nearest_node
+  
+    nearest_node
+  end
+  
+  def get_distance_between_two_nodes(first_node, second_node) 
+    puts "Distancia do #{first_node} pro #{second_node} => #{@graph[first_node][second_node]}"
+    return @graph[first_node][second_node]
+  end
+  
+  private
+
   def calculate_path(path)
     # path = array com a permutação atual dos nós (ordem que os nós vão ser visitados)
     
@@ -61,18 +111,12 @@ class TSP
     total_cost
   end
   
-  def held_karp_tsp
-    nodes = (0...@nodes_quantity).to_a  
-    starting_node = 0
-    
-    memo_table = Array.new(@nodes_quantity) { Array.new(2**@nodes_quantity, nil) }
-    
-  end
+
   
 end
 
 tsp = TSP.new
-# tsp.read_file("./resources/tsp1_253.txt") # custo ótimo é 253
-tsp.read_file("./resources/tsp_exemplo.txt") # custo ótimo é 253
+tsp.read_file("./TSP/resources/tsp1_253.txt") # custo ótimo é 253
+# tsp.read_file("./TSP/resources/tsp_exemplo.txt") # custo ótimo é 253
 # result = tsp.brute_force_tsp
-tsp.held_karp_tsp
+tsp.nearest_neighbor_tsp
